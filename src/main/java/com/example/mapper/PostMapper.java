0430282e38234948760a1c92c31a7b85d1cdaee2
Hyoin Lee <hiliee09@naver.com>
2024-12-15 23:17:32 +0900

    [update] mapper

--- src/main/java/com/example/mapper/PostMapper.java
+++ src/main/java/com/example/mapper/PostMapper.java
@@ -2,6 +2,7 @@ package com.example.mapper;
 
 import com.example.dto.PostDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -15,4 +16,20 @@ public interface PostMapper {
     void deletePostById(int id);
     List<PostDTO> selectAllPosts();
     int getPostOwnerId(int postId);
+
+    String getFileUrlByPostId(@Param("postId") int postId);
+
+    // 상태가 REQUESTING_TRANSFER인 게시글 조회
+    List<PostDTO> selectPostsByStatusRequesting();
+
+    // 상태가 TRANSFERRING인 게시글 조회
+    List<PostDTO> selectPostsByStatusTransferring();
+
+    // 상태가 COMPLETED인 게시글 조회
+    List<PostDTO> selectPostsByStatusCompleted();
+
+    // 제목에 특정 글자가 포함된 게시물 검색
+    List<PostDTO> searchPostsByTitle(@Param("keyword") String keyword);
+
+
 }
